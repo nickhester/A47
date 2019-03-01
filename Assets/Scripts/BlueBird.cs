79a79d74d3b985b9970800855265a6bbb05e27d8
Nick Hester <nick.d.hester@accruent.com>
2019-02-28 22:19:19 -0600

    first project commit!

---
+++ Assets/Scripts/BlueBird.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using vector = UnityEngine.Vector2;
+
+public class BlueBird : MonoBehaviour {
+
+	Rigidbody2D bird;
+	Vector2 up = vector.up * 5;
+	Vector2 down = vector.down * 5;
+	Vector2 left = vector.left * 5;
+	Vector2 right = vector.right * 5;
+	float timeFlying = 0.0f;
+	float timeToChangeDirection = 1.0f;
+	Vector2 currentDirection;
+
+	void Start () {
+		bird = gameObject.GetComponent<Rigidbody2D>();
+		currentDirection = up;
+	}
+	
+
+	void Update () {
+		timeFlying += Time.deltaTime;
+		if (timeFlying > timeToChangeDirection)
+		{
+			timeFlying = 0.0f;
+			int direction = Random.Range(0, 4);
+			switch (direction)
+			{
+				case 0:
+					currentDirection = up;
+					break;
+				case 1:
+					currentDirection = down;
+					break;
+				case 2:
+					currentDirection = left;
+					break;
+				case 3:
+					currentDirection = right;
+					break;
+				default:
+					currentDirection = right;
+					break;
+			}
+		}
+		
+		bird.AddRelativeForce(currentDirection);
+	}
+
+	// Helpers - Don't touch! =================
+	bool OnKey(string keyName) { return Input.GetKey(keyName); }
+}
---
+++ Assets/Scripts/RedBird.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using vector = UnityEngine.Vector2;
+
+public class RedBird : MonoBehaviour {
+
+	Rigidbody2D bird;
+	Vector2 up = vector.up * 20;
+	Vector2 down = vector.down * 20;
+	Vector2 left = vector.left * 20;
+	Vector2 right = vector.right * 20;
+
+	void Start () {
+		bird = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+
+	void Update () {
+		
+		if (OnKey("up")){
+			bird.AddForce(up);
+		}
+		if (OnKey("down")){
+			bird.AddForce(down);
+		}
+		if (OnKey("left")){
+			bird.AddForce(left);
+		}
+		if (OnKey("right")){
+			bird.AddForce(right);
+		}
+	}
+
+	// Helpers - Don't touch! =================
+	bool OnKey(string keyName) { return Input.GetKey(keyName); }
+}
